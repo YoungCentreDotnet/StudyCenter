d0f85996be7ff691228ec5ea9fde9950b7f00132
USER-4 <USER-4@USER-4>
2023-10-23 19:36:09 +0500

    DATA: Initialize Admin.cs and User.cs in Model Folder and Reposytory Services

---
+++ StudyCenter.Backend/Models/Admin.cs
@@ -0,0 +1,22 @@
+﻿using LinqToDB.Mapping;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace StudyCenter.Backend.Models
+{
+    public class Admin
+    {
+        [Identity]
+        public int Id { get; set; }
+        [Required]
+        public string FirsName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Login { get; set; }
+        [PasswordPropertyText]
+        [MaxLength(8)]
+        public string Password { get; set; }
+    }
+}
