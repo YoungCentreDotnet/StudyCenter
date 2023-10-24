06650b2a0ee130bc0ea6acdec3f34fe4acb7ac14
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-10-24 17:40:26 +0500

    Add StudyDbContext in Program.cs

--- StudyCenter.Backend/Program.cs
+++ StudyCenter.Backend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using StudyCenter.Backend.DataLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StudyDbContext>(op =>
+                op.UseSqlServer(builder.Configuration.GetConnectionString("StudyConectionString")));
 
 var app = builder.Build();
 
