7c121cec8d02ad92bfb24c7a649e5dde7c3cb358
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-11-07 20:20:50 +0500

    MIGRATION: Full Models Created

---
+++ StudyCenter.Backend/Controllers/SubjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyCenter.Backend.Controllers
+{
+    public class SubjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
